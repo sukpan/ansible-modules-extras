a6a13f7a009ba18ad73a98416f10767ed43416d1
Paul Northrop <paul.northrop@sas.com>
2016-11-08 07:30:27 +1100

    Initial commit of win_wait_for module

---
+++ windows/win_wait_for.ps1
@@ -0,0 +1,427 @@
+#!powershell
+# win_wait_for
+# Author: Paul Northrop (GitHub: @sukpan), SAS Institute, Inc. (GitHub: @sassoftware)
+#
+# Purpose: To mimic the core ansible module "wait_for" on Windows platforms
+#
+#
+# Copyright (c) 2016 SAS Institute, Inc.
+#
+# This module is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# WANT_JSON
+# POWERSHELL_COMMON
+
+
+$ErrorActionPreference = "Stop"
+
+# ================================================================
+# Functions
+# ================================================================
+
+Function getConnDetails {
+    param(  [string]$TargetHost,
+            [string]$TargetPort,
+            [string]$Timeout)
+
+    $outputobj = New-Object PSObject
+    $outputobj | Add-Member -MemberType NoteProperty -Name TargetHostName -Value $TargetHost            
+
+    if(Test-Connection -ComputerName $TargetHost -count 2 -ErrorAction SilentlyContinue) {            
+        $outputobj | Add-Member -MemberType NoteProperty -Name TargetHostStatus -Value "ONLINE"            
+    } else {            
+        $outputobj | Add-Member -MemberType NoteProperty -Name TargetHostStatus -Value "OFFLINE"            
+    }            
+
+    $outputobj | Add-Member -MemberType NoteProperty -Name PortNumber -Value $TargetPort       
+
+    $Socket = New-Object System.Net.Sockets.TCPClient            
+    $Connection = $Socket.BeginConnect($Targethost,$TargetPort,$null,$null)            
+    $Connection.AsyncWaitHandle.WaitOne($Timeout,$false)  | Out-Null            
+
+    if($Socket.Connected -eq $true) {            
+        $outputobj | Add-Member -MemberType NoteProperty -Name ConnectionStatus -Value $true            
+    } else {            
+        $outputobj | Add-Member -MemberType NoteProperty -Name ConnectionStatus -Value $false           
+    }            
+
+    $Socket.Close | Out-Null                        
+    return $outputobj           
+
+}
+
+
+Function getConnectionCount() {
+
+    param(  [string]$ExcludeHosts, 
+            [string]$LocalPort)
+
+    [string[]] $hosts=($ExcludeHosts).Split(',') 
+
+    [string[]]$ips = ""
+    foreach ($myhost in $hosts)  
+    {
+        try
+        {
+            [string[]]$ipaddress = [System.Net.Dns]::GetHostAddresses($myhost) 
+        
+            foreach ($ip in $ipaddress)
+                {
+                    $ips+=$ip
+                }
+        }
+        catch
+        {
+            continue
+        }
+    }
+    $excl=  $ips | sort-object | Get-Unique
+
+    $connCount=-1
+
+
+    $conns=Get-NetTCPConnection -LocalPort $LocalPort -ErrorAction SilentlyContinue
+
+    $conns = $conns | Where-Object { $_.State -notin "Listen" -and $_.RemoteAddress -notin $excl}
+    $connCount=($conns.State).Count
+
+    return $connCount
+
+}
+
+# ================================================================
+# Parameters
+# ================================================================
+$params = Parse-Args $args;
+
+# result
+$result = New-Object PSObject;
+Set-Attr $result "changed" $false;
+Set-Attr $result "failed" $false;
+
+# connect_timeout
+$connect_timeout = Get-AnsibleParam -obj $params -name "connect_timeout" -default "5"
+
+# delay - time to wait before checking required condition
+$delay = Get-AnsibleParam -obj $params -name "delay" -default "0"
+
+# exclude_hosts
+$exclude_hosts = Get-AnsibleParam -obj $params -name "exclude_hosts" 
+
+## If exclude_hosts is an array of strings, convert to a comma separated list
+If ($exclude_hosts -is [system.array])
+    {
+        $exclude_hosts = $exclude_hosts -Join ','
+    }
+
+# _host - called _host as host is a reserved object in PowerShell
+$_host = Get-AnsibleParam -obj $params -name "host" -default "127.0.0.1"
+
+# path
+$path = Get-AnsibleParam -obj $params -name "path"
+
+# name - alias for path
+$name = Get-AnsibleParam -obj $params -name "name"
+
+# path
+$path = Get-AnsibleParam -obj $params -name "path"
+
+# port
+$port = Get-AnsibleParam -obj $params -name "port"
+
+# search_regex
+$search_regex = Get-AnsibleParam -obj $params -name "search_regex"
+
+# state
+$state = Get-AnsibleParam -obj $params -name "state" -default "started" -ValidateSet "present", "started", "stopped", "absent", "drained" -resultobj $result
+
+# timeout
+$timeout = Get-AnsibleParam -obj $params -name "timeout" -default "300"
+
+# sleep - time to wait between each check of required condition - default 1 second
+$sleep = Get-AnsibleParam -obj $params -name "sleep" -default "1"
+
+
+
+
+
+# ================================================================
+# Validation
+# ================================================================
+
+If ($delay -lt 0)
+    {
+        Fail-Json $result  "delay cannot be negative"
+    }
+
+If ($timeout -lt 0)
+    {
+        Fail-Json $result  "timeout cannot be negative"
+    }
+
+If ($delay -ge $timeout)
+    {
+        Fail-Json $result  "delay must be less than timeout"
+    }   
+
+If ($name -and $path)
+    {
+        Fail-Json $result  "name and path cannot both be specified"
+    }
+
+If ( ($name -or $path) -and !($state -or $search_regex) )
+    {
+        # set path to the value of name, as the main code uses path variable
+        Fail-Json $result  "you must specify either state or search_regex when using a file name or path"
+    }    
+
+If ($name)
+    {
+        # set path to the value of name, as the main code uses path variable
+        $path = "$name"
+    }
+
+If ($port -and $path)
+    {
+        Fail-Json $result  "port and path parameter can not both be passed to the win_wait_for module"
+    }
+
+If ($path -and ($state -eq "stopped") )
+    {
+        Fail-Json $result  "state=stopped should only be used for checking a port in the win_wait_for module"
+    }
+
+If ($path -and ($state -eq "drained") )
+    {
+        Fail-Json $result  "state=drained should only be used for checking a port in the win_wait_for module"
+    }
+
+If ($port -and $path)
+    {
+        Fail-Json $result  "port and path parameter can not both be passed to win_wait_for module"
+    }
+
+If ($exclude_hosts -and !($state -eq "drained") )
+    {
+        Fail-Json $result  "exclude_hosts should only be with state=drained with win_wait_for module"
+    }
+
+If (!($port) -and ($state -eq "drained") )
+    {
+        Fail-Json $result  "port must be provided for state=drained with win_wait_for module"
+    }
+
+# ================================================================
+# Variable initialisation
+# ================================================================
+
+$done=$false
+
+$iterations=0
+
+
+# ================================================================
+# Main section
+# ================================================================
+Try {
+    $start = Get-Date
+    $stop_at = $start.AddSeconds($timeout)
+    
+    # Don't sleep if no path or port is set as all we want to do is a pure wait. 
+    If ( ($delay -gt 0) -and ($path -or $port) ) 
+        {
+            sleep $delay
+            Set-Attr $result "slept" $true
+        }
+
+    If ( !($path) -and !($port) -and ($state -ne "drained") )
+        {
+            # just wait
+            sleep $timeout
+            $done = $true;
+            
+            # set iterations to 1 so the status is changed
+            $iterations = 1
+            $done = $true
+        }
+    ElseIf ( ($state -eq "absent") -or ($state -eq "stopped") )
+        {
+            While ( !($done) -and ( (Get-Date) -lt $stop_at) )
+                {
+                    If ($path) 
+                        {
+                            # wait for file to be removed
+                            If (!(Test-Path -Path $path -PathType Leaf)) 
+                                {
+                                    $done = $true;
+                                    break
+                                }
+                        }
+                    If ($port) 
+                        {
+                            # wait for port to be unresponsive
+                            $portCheck = getConnDetails -TargetHost $_host -TargetPort $port -Timeout $connect_timeout
+                            if ($portCheck.TargetHostStatus -eq "OFFLINE")
+                                {
+                                    Fail-Json $result  "host '$_host' cannot be resolved"
+                                }
+                            if ( !($portCheck.ConnectionStatus) )
+                                {
+                                    $done = $true;
+                                    break
+                                }
+
+                        }
+                        
+                    sleep $sleep
+                    $iterations = $iterations + 1                        
+                }
+                
+                If ( !$done )
+                    {
+                        $finished = Get-Date
+                        $elapsed = $finished - $start
+                                                
+                        Set-Attr $result "elapsed" $elapsed.Seconds
+
+                        If ($path)
+                            {
+                                Set-Attr $result "path" $path
+                                Fail-Json $result  "timeout exceeded waiting for $path to be absent"
+                            }
+                        If ($port)
+                            {
+                                Set-Attr $result "port" $port
+                                Fail-Json $result  "timeout exceeded waiting for $_host on $port to be stopped"
+                            }
+                    }
+        }
+    ElseIf ( ($state -eq "started") -or ($state -eq "present") )
+        {
+            While ( !($done) -and ( (Get-Date) -lt $stop_at) )
+                {
+                    If ($path) 
+                        {
+                            # wait for file to exist
+                            If ((Test-Path -Path $path -PathType Leaf)) 
+                                {
+                                    If ($search_regex) 
+                                        {                
+                                            # read the files contents and match against the regular expression
+                                            $contents = Get-Content $path -Raw | Select-String $search_regex
+                                            If ($contents -ne $null)
+                                                {
+                                                    $done = $true;
+                                                    break
+                                                }
+                                        }
+                                    Else 
+                                        {
+                                        $done = $true;
+                                        break
+                                        }
+                                }
+                        }
+                    If ($port) 
+                        {
+                            # wait for port to respond
+                            $portCheck = getConnDetails -TargetHost $_host -TargetPort $port -Timeout $connect_timeout
+                            if ($portCheck.TargetHostStatus -eq "OFFLINE")
+                                {
+                                    Fail-Json $result  "host '$_host' cannot be resolved"
+                                }
+                            if ( $portCheck.ConnectionStatus )
+                                {
+                                    $done = $true;
+                                    break
+                                }
+
+                        }
+                        
+                    sleep $sleep
+                    $iterations = $iterations + 1                        
+                }
+                
+                If ( !$done )
+                    {
+                        $finished = Get-Date
+                        $elapsed = $finished - $start
+                                                
+                        Set-Attr $result "elapsed" $elapsed.Seconds
+
+                        If ($path -and !($search_regex) )
+                            {
+                                Set-Attr $result "path" $path
+                                Fail-Json $result  "timeout exceeded waiting for $path to be present"
+                            }
+                        If ($port)
+                            {
+                                Set-Attr $result "port" $port
+                                Fail-Json $result  "timeout exceeded waiting for $_host on $port to be present"
+                            }
+                        If ($path -and $search_regex)
+                            {
+                                Set-Attr $result "path" $path
+                                Fail-Json $result  "timeout exceeded waiting for line regular expression match in $path to be present"
+                            }                            
+                    }        
+        }
+    ElseIf ( ($state -eq "drained") )
+        {
+            ### wait until all active connections are gone
+            
+            While ( !($done) -and ( (Get-Date) -lt $stop_at) )
+                {
+                    $active_connections = getConnectionCount -ExcludeHosts $exclude_hosts -LocalPort $port
+                    If ($active_connections -eq 0)
+                        {
+                            $done = $true
+                            break
+                        }
+                    sleep $sleep
+                    $iterations = $iterations + 1                                        
+                }
+            If (!$done)
+                {
+                    Set-Attr $result "port" $port
+                    Set-Attr $result "exclude_hosts" $exclude_hosts
+                    Fail-Json $result  "timeout exceeded waiting for $_host on $port to be drained"                
+                }        
+        }
+
+    $finished = Get-Date
+    $elapsed = $finished - $start
+
+    Set-Attr $result "elapsed" $elapsed.Seconds
+
+    If ($done -and $iterations -eq 0) 
+        {
+            Set-Attr $result "changed" $false;
+        }    
+    ElseIf ($done) 
+        {
+            Set-Attr $result "changed" $true;
+        }    
+    Else 
+        {
+            Fail-Json $result  "timeout exceeded waiting for condition"
+        }
+
+
+}
+Catch {
+    $ErrorMessage = $_.Exception.Message
+    Fail-Json $result "an unhandled error occured. Error message is '$ErrorMessage'."
+}
+
+Exit-Json $result
---
+++ windows/win_wait_for.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Title: win_wait_for.ps1
+# Author: Paul Northrop (GitHub: @sukpan), SAS Institute, Inc. (GitHub: @sassoftware)
+#
+# Purpose: To mimic the core ansible module "wait_for" on Windows platforms
+#
+#
+# Copyright (c) 2016 SAS Institute, Inc.
+#
+# This module is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+
+DOCUMENTATION = '''
+---
+module: win_wait_for
+version_added: "2.3"
+description:
+  - You can wait for a set amount of time C(timeout), this is the default if nothing is specified.
+  - Waiting for a port to become available is useful for when services are not immediately available after their init scripts return which is true of certain Java application servers. 
+  - This module can also be used to wait for a regex match a string to be present in a file.
+  - This module can also be used to wait for a file to be available or absent on the filesystem.
+  - This module can also be used to wait for active connections to be closed before continuing, useful if a node is being rotated out of a load balancer pool.
+options:
+  host:
+    description:
+      - A resolvable hostname or IP address to wait for
+    required: false
+    default: "127.0.0.1"
+  timeout:
+    description:
+      - maximum number of seconds to wait for
+    required: false
+    default: 300
+  connect_timeout:
+    description:
+      - maximum number of seconds to wait for a connection to happen before closing and retrying
+    required: false
+    default: 5
+  delay:
+    description:
+      - number of seconds to wait before starting to poll
+    required: false
+    default: 0
+  port:
+    description:
+      - port number to poll
+    required: false
+  state:
+    description:
+      - either C(present), C(started), or C(stopped), C(absent), or C(drained)
+      - When checking a port C(started) will ensure the port is open, C(stopped) will check that it is closed, C(drained) will check for active connections
+      - When checking for a file or a search string C(present) or C(started) will ensure that the file or string is present before continuing, C(absent) will check that file is absent or removed
+    choices: [ "present", "started", "stopped", "absent", "drained" ]
+    default: "started"
+  path:
+    required: false
+    description:
+      - path to a file on the filesytem that must exist before continuing
+  search_regex:
+    required: false
+    description:
+      - Can be used to match a string in a file. Defaults to a multiline regex.
+  exclude_hosts:
+    required: false
+    description:
+      - comma separated list or array of hosts or IPs to ignore when looking for active TCP connections for C(drained) state
+  sleep:
+    required: false
+    default: 1
+    description:
+      - Number of seconds between each poll
+notes:
+  - Cannot match a string in a socket connection.
+  - I(delay) parameter MUST be less than I(timeout) parameter or an error will be thrown
+  - When I(path) or I(name) is not provided, the I(delay) parameter is ignored so the total wait time is I(timeout).
+  - I(search_regex) cannot be used when reading from a socket. This may be added in a later release.
+requirements: []
+author:
+    - "Paul Northrop (@sukpan)"
+    - "Acknowledgement: wait_for: Jeroen Hoekx (@jhoekx)"
+    - "Acknowledgement: wait_for: John Jarvis (@jarv)"
+    - "Acknowledgement: wait_for: Andrii Radyk (@AnderEnder)"
+'''
+
+EXAMPLES = '''
+
+# just wait for 30 seconds
+- win_wait_for: timeout=30
+
+# just wait for 30 seconds - in this case, delay is ignored
+- win_wait_for: timeout=30 delay=20
+
+# wait for a file to exist, timeout after 10 minutes but don't perform first check for 1 minute
+- win_wait_for: name='C:\Temp\somefile.txt' timeout=600 state=present delay=60
+
+# wait for a file to be removed, wait 10 seconds between each check and timeout after 30 minutes
+- win_wait_for: path='C:\Temp\somefile.txt' timeout=1800 state=absent sleep=10
+
+# wait for a file to contain a line starting with Hello using default timeout of 5 minutes
+- win_wait_for: path='C:\Temp\somefile.txt' search_regex='^Hello'
+
+# wait up to 10 minutes for HTTP port (80) to be free
+- win_wait_for: timeout=600 port=80 host=localhost state=stopped
+
+'''
